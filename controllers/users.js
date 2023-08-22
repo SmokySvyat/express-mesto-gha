17cc10b8f79eccb561f1cac52152df4cf7d92c94
SvyatLinux <swcnova@gmail.com>
2023-08-22 20:42:40 +0300

    fixing5

--- controllers/users.js
+++ controllers/users.js
@@ -21,7 +21,7 @@ const getUsers = (req, res, next) => {
 const findById = (req, res, next, id) => {
   User.findById(id)
     .orFail(new NotFound(`Пользователь по указанному id: ${id} не найден`))
-    .then((user) => res.status(STATUS_OK).send(user))
+    .then((user) => res.send(user))
     .catch(next);
 };
 
