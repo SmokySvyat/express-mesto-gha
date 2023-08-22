517c306cd3d413d013442124adc692ae5bdb1481
SvyatLinux <swcnova@gmail.com>
2023-08-22 20:25:51 +0300

    fixing3

--- app.js
+++ app.js
@@ -3,18 +3,19 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const errorHandler = require('./middlewres/error');
 const router = require('./routes');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(router);
 
 app.use(errors());
+app.use(errorHandler);
 
 mongoose.connect('mongodb://localhost:27017/testdb')
   .then(() => console.log('Connected to the data base'))
